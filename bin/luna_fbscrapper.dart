0e0f9b418674341bfef8e2b3153562e2eb8f0d21
Mahmoud Ashour <mashour365@gmail.com>
2021-08-05 21:34:25 +0200

    Fixed fb video not found error

--- bin/luna_fbscrapper.dart
+++ bin/luna_fbscrapper.dart
@@ -4,6 +4,22 @@ import 'dart:io';
 import 'package:web_scraper/web_scraper.dart';
 
 String? input() => stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
+String? _facebookVideoLink;
+late WebScraper webScraper;
+
+void _findVideoUrl({required int j}) {
+  while (_facebookVideoLink == null) {
+    try {
+      var _facebookVideoDecoded = json.decode(webScraper.getAllScripts()[j]);
+      _facebookVideoLink = _facebookVideoDecoded['contentUrl'];
+      _colorfulPrint(
+          message: 'Facebook Video URL : $_facebookVideoLink', color: 'green');
+      j++;
+    } on FormatException catch (e) {
+      j++;
+    }
+  }
+}
 
 void _colorfulPrint({String color = 'red', message}) {
   var _colorScheme = <String, String>{
@@ -21,29 +37,27 @@ void _colorfulPrint({String color = 'red', message}) {
 }
 
 void main(List<String> arguments) async {
-  late WebScraper webScraper;
   _colorfulPrint(message: 'Enter your facebook link', color: 'magenta');
   final _facebookLink = input();
   try {
     webScraper = WebScraper();
     // await webScraper.loadWebPage('UefaSarcasmSociety/posts/4590156297703013');
+    _colorfulPrint(message: 'Loading your facebook video', color: 'yellow');
+
     await webScraper.loadFullURL('$_facebookLink');
-    var _facebookVideoDecoded = json.decode(webScraper.getAllScripts()[3]);
-
-    final _facebookVideoLink = _facebookVideoDecoded['contentUrl'];
-    print('Facebook Video URL : $_facebookVideoLink ');
+    _findVideoUrl(j: 0);
   } on WebScraperException catch (ex, _) {
-    print(ex.errorMessage());
+    _colorfulPrint(message: ex.errorMessage(), color: 'red');
+    _colorfulPrint(message: "Couldn't find video!", color: 'red');
   }
-
 // Alternative way to get the source of Facebook video
   // try {
-  //   var _link = webScraper
-  //       .getPageContent()
-  //       .split('<a href="/video_redirect/?src=')[1]
-  //       .split('"')[0];
+  // var _link = webScraper
+  //     .getPageContent()
+  //     .split('<a href="/video_redirect/?src=')[1]
+  //     .split('"')[0];
 
-  //   var _formattedLinkVideo = Uri.decodeComponent(_link);
+  // var _formattedLinkVideo = Uri.decodeComponent(_link);
   //   print(_formattedLinkVideo);
   // } catch (e) {
   //   print('couldnt find video');
